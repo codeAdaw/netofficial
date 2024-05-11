3a4542c451ebe080891658c6abc6b9a3c0470b64
codeAdaw <18810539364@sina.cn>
2024-05-11 14:47:18 +0800

    add scroll
    add scroll

--- .history/app/page_20240511144432.tsx
+++ .history/app/page_20240511144432.tsx
@@ -76,6 +76,7 @@ export default function Home() {
     <>
      { initDone &&
         <main className="bg-block h-fit w-full flex-1 absolute top-0 box-border bg-[#100F22] relative selector">
+            <div className="h-full w-full">
                 <ReactFullpage
                     //fullpage options
                     licenseKey = {'YOUR_KEY_HERE'}
@@ -97,15 +98,6 @@ export default function Home() {
                     render={({ state, fullpageApi }) => {
                         return (
                             <ReactFullpage.Wrapper>
-                                  <div id="bannerWrap" className="section mx-auto w-full lg:h-screen sm:h-banner bg-cover relative">
-                                        {/* <Suspense> */}
-                                            <Spline className="h-full w-full"  scene="https://prod.spline.design/HgOIH-tqLnthZoKa/scene.splinecode" />
-                                        {/* </Suspense> */}
-                                        <div className="lg:w-1/2 lg:h-24 md:h-96 m-auto text-center absolute sm:top-96 lg:top-0 bottom-0 left-0 right-0">
-                                            <h1  id="tttt" className="lg:text-8xl md:text-6xl sm:text-5xl sm:mt-24 lg:mt-0 font-extrabold text-write-900"></h1>
-                                            <p className="mt-4 text-write-500 lg:text-4xl md:text-2xl">{intl.get("SLOGEN")}</p>
-                                        </div>
-                                </div>
                             {/* part 1 */}
                                 <div className="section bg-[#100F22] mx-auto w-full lg:h-screen sm:h-auto bg-cover flex lg:flex-row sm:flex-col-reverse sm:text-center items-center lg:px-28 lg:gap-x-14 sm:px-mspace absolute">
                                     <img className="absolute left-0 top-96 lg:w-120 z-0"  src='/images/bgleft.png' alt=""/>
@@ -203,6 +195,7 @@ export default function Home() {
                             </ReactFullpage.Wrapper>
                         );}
                     }/>
+            </div>
         </main>
      }
     </>
--- .history/app/page_20240511144500.tsx
+++ .history/app/page_20240511144500.tsx
@@ -97,15 +97,15 @@ export default function Home() {
                     render={({ state, fullpageApi }) => {
                         return (
                             <ReactFullpage.Wrapper>
-                                  <div id="bannerWrap" className="section mx-auto w-full lg:h-screen sm:h-banner bg-cover relative">
-                                        {/* <Suspense> */}
-                                            <Spline className="h-full w-full"  scene="https://prod.spline.design/HgOIH-tqLnthZoKa/scene.splinecode" />
-                                        {/* </Suspense> */}
-                                        <div className="lg:w-1/2 lg:h-24 md:h-96 m-auto text-center absolute sm:top-96 lg:top-0 bottom-0 left-0 right-0">
-                                            <h1  id="tttt" className="lg:text-8xl md:text-6xl sm:text-5xl sm:mt-24 lg:mt-0 font-extrabold text-write-900"></h1>
-                                            <p className="mt-4 text-write-500 lg:text-4xl md:text-2xl">{intl.get("SLOGEN")}</p>
-                                        </div>
-                                </div>
+                                  <div id="bannerWrap" className="mx-auto w-full lg:h-screen sm:h-banner bg-cover relative">
+                    <Suspense>
+                        <Spline className="h-full w-full"  scene="https://prod.spline.design/HgOIH-tqLnthZoKa/scene.splinecode" />
+                    </Suspense>
+                    <div className="lg:w-1/2 lg:h-24 md:h-96 m-auto text-center absolute sm:top-96 lg:top-0 bottom-0 left-0 right-0">
+                            <h1  id="tttt" className="lg:text-8xl md:text-6xl sm:text-5xl sm:mt-24 lg:mt-0 font-extrabold text-write-900"></h1>
+                            <p className="mt-4 text-write-500 lg:text-4xl md:text-2xl">{intl.get("SLOGEN")}</p>
+                    </div>
+            </div>
                             {/* part 1 */}
                                 <div className="section bg-[#100F22] mx-auto w-full lg:h-screen sm:h-auto bg-cover flex lg:flex-row sm:flex-col-reverse sm:text-center items-center lg:px-28 lg:gap-x-14 sm:px-mspace absolute">
                                     <img className="absolute left-0 top-96 lg:w-120 z-0"  src='/images/bgleft.png' alt=""/>
